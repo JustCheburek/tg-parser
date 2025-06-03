5e4e0d18543452a9df0f5aae8e616fd1b5693db8
JustCheburek <artemiznakhdki@gmail.com>
2025-06-03 23:07:23 +1000

    0.0.0.0

--- src/api/index.ts
+++ src/api/index.ts
@@ -20,6 +20,6 @@ app.get('/', (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`API сервер запущен на http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`API сервер запущен на http://0.0.0.0:${PORT}`);
 }); 
